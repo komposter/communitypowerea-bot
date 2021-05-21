3ee6a20df371eb91c1e01333fa9910cc51371dc9
Jia Rong <jiarong@ezsofe.com>
2021-05-22 06:57:32 +0800

    - Fix forced reply with selective not working.

--- index.js
+++ index.js
@@ -66,11 +66,10 @@ bot.command('forum', async (ctx) => {
 });
 
 const help = async (ctx) => {
-    const { update: { message: { text, from: { id } } } } = ctx;
-
+    const { update: { message: { text, from: { id, username } } } } = ctx;
     if (text === `/help@${process.env.BOT_USERNAME}` || text === "/help") {
         if (cache.set(id, "PENDING_QUESTION")) {
-            await ctx.reply("Please, specify your question.", Markup.forceReply(true).selective(true));
+            await ctx.reply(`Hello @${username}! Please, specify your question.`, Markup.forceReply(true).selective(true));
         } else {
             await ctx.reply("Please, specify your question: /help <your question>");
         }
