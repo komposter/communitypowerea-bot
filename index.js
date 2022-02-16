22f6fe47e3020a89f43d4bb39d876a231203bfa1
kompsterius <komposter@gmail.com>
2022-02-17 01:51:50 +0200

    Revert "- Added scheduler [TEST]"
    This reverts commit 73aa2f487edadcf5bc12fae1f51ee2ca0ac978eb.

--- index.js
+++ index.js
@@ -2,101 +2,126 @@ import dotenv from "dotenv";
 import { Telegraf, Markup } from 'telegraf'
 import axios from 'axios';
 import NodeCache from "node-cache";
-import schedule from 'node-schedule';
 
-const message = `🌈 How to support the project:
-- use the EA. Test it, use it live!
-- suggest and discuss improvements, vote for those you like,
-- share your settings and ideas,
-- leave your feedback on mql5 market — https://www.mql5.com/en/market/product/48493#!tab=reviews
-- became a patron on https://www.patreon.com/CommunityPower
-- open trading account using my IB-link (find it in the google doc)`;
+const cache = new NodeCache({ stdTTL: 300, checkperiod: 320 });
 
-(async () => {
+dotenv.config();
 
-    const cache = new NodeCache({ stdTTL: 300, checkperiod: 320 });
+const escapeCharacters = (str) => {
+    return new String(str)
+        .replace(/\_/g, "\\_")
+        .replace(/\*/g, "\\*")
+        .replace(/\[]/g, "\\[")
+        .replace(/\]/g, "\\]")
+        .replace(/\(/g, "\\(")
+        .replace(/\)/g, "\\)")
+        .replace(/\~/g, "\\~")
+        .replace(/\`/g, "\\`")
+        .replace(/\>/g, "\\>")
+        .replace(/\#/g, "\\#")
+        .replace(/\+/g, "\\+")
+        .replace(/\-/g, "\\-")
+        .replace(/\=/g, "\\=")
+        .replace(/\|/g, "\\|")
+        .replace(/\{/g, "\\{")
+        .replace(/\}/g, "\\}")
+        .replace(/\./g, "\\.")
+        .replace(/\!/g, "\\!");
+}
 
-    dotenv.config();
+const performSearch = async (query) => {
+    return axios.get(`https://communitypowerea.userecho.com/api/v2/forums/7/topics/search.json?query=${query}&access_token=${process.env.USERECHO_TOKEN}`);
+}
 
-    const escapeCharacters = (str) => {
-        return new String(str)
-            .replace(/\_/g, "\\_")
-            .replace(/\*/g, "\\*")
-            .replace(/\[]/g, "\\[")
-            .replace(/\]/g, "\\]")
-            .replace(/\(/g, "\\(")
-            .replace(/\)/g, "\\)")
-            .replace(/\~/g, "\\~")
-            .replace(/\`/g, "\\`")
-            .replace(/\>/g, "\\>")
-            .replace(/\#/g, "\\#")
-            .replace(/\+/g, "\\+")
-            .replace(/\-/g, "\\-")
-            .replace(/\=/g, "\\=")
-            .replace(/\|/g, "\\|")
-            .replace(/\{/g, "\\{")
-            .replace(/\}/g, "\\}")
-            .replace(/\./g, "\\.")
-            .replace(/\!/g, "\\!");
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+await bot.telegram.setMyCommands([
+    // {
+    //     command: "start",
+    //     description: "Welcome to CommunityPower Help Bot"
+    // },
+    {
+        command: "help",
+        description: "<your question here> (after space)"
+    },
+    {
+        command: "guide",
+        description: "Official CommunityPower EA documentation"
+    },
+    {
+        command: "forum",
+        description: "Community set-files and ideas (UserEcho forum)"
+    },
+    {
+        command: "version",
+        description: "Bot version."
     }
+]);
 
-    const performSearch = async (query) => {
-        return axios.get(`https://communitypowerea.userecho.com/api/v2/forums/7/topics/search.json?query=${query}&access_token=${process.env.USERECHO_TOKEN}`);
-    }
+bot.command('start', async (ctx) => {
+    await ctx.reply("This bot will search help topics on the community forum.\n\nUse '/help <your question>' command to ask any questions you have!");
+});
 
-    const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.command('guide', async (ctx) => {
+    await ctx.reply("https://docs.google.com/document/d/1ww1M97H54IBwtCKZDhxtqsTsrtEMKofXHMEWMGCyZNs");
+});
 
-    const job = schedule.scheduleJob('/1 * * * *', function () {
-        ctx.telegram.sendMessage("@juniarz", message);
-        //ctx.telegram.sendMessage("@CommunityPowerEA", message);
-    });
+bot.command('forum', async (ctx) => {
+    await ctx.reply("https://communitypowerea.userecho.com/");
+});
 
-    await bot.telegram.setMyCommands([
-        // {
-        //     command: "start",
-        //     description: "Welcome to CommunityPower Help Bot"
-        // },
-        {
-            command: "help",
-            description: "<your question here> (after space)"
-        },
-        {
-            command: "guide",
-            description: "Official CommunityPower EA documentation"
-        },
-        {
-            command: "forum",
-            description: "Community set-files and ideas (UserEcho forum)"
-        },
-        {
-            command: "version",
-            description: "Bot version."
+const help = async (ctx) => {
+    const { update: { message: { text, from: { id, username } } } } = ctx;
+    if (text === `/help@${process.env.BOT_USERNAME}` || text === "/help") {
+        if (cache.set(id, "PENDING_QUESTION")) {
+            await ctx.reply(`Hello ${username && `@${username}`}! Please, specify your question.`, Markup.forceReply(true).selective(true));
+        } else {
+            await ctx.reply("Please, specify your question: /help <your question>");
         }
-    ]);
 
-    bot.command('start', async (ctx) => {
-        await ctx.reply("This bot will search help topics on the community forum.\n\nUse '/help <your question>' command to ask any questions you have!");
-    });
+        return;
+    }
 
-    bot.command('guide', async (ctx) => {
-        await ctx.reply("https://docs.google.com/document/d/1ww1M97H54IBwtCKZDhxtqsTsrtEMKofXHMEWMGCyZNs");
-    });
+    const query = text.replace(`\/help@${process.env.BOT_USERNAME} `, "").replace("\/help ", "");
 
-    bot.command('forum', async (ctx) => {
-        await ctx.reply("https://communitypowerea.userecho.com/");
-    });
+    const response = await performSearch(query);
 
-    const help = async (ctx) => {
-        const { update: { message: { text, from: { id, username } } } } = ctx;
-        if (text === `/help@${process.env.BOT_USERNAME}` || text === "/help") {
-            if (cache.set(id, "PENDING_QUESTION")) {
-                await ctx.reply(`Hello ${username && `@${username}`}! Please, specify your question.`, Markup.forceReply(true).selective(true));
-            } else {
-                await ctx.reply("Please, specify your question: /help <your question>");
-            }
+    if (response.data.status === 'success') {
+        const results = response.data.data;
 
+        if (results.length === 0) {
+            await ctx.reply(`No topics related to '${query}' found. Try to rephrase!`);
             return;
         }
+
+        let message = `Help topics related to '${query}':\n`
+        for (let i = 0; i < Math.min(results.length, 3); i++) {
+            const _result = results[i];
+            message += `\\- [${escapeCharacters(_result.header)}]\(${escapeCharacters(_result.url)}\)\n`;
+        }
+
+        if (results.length > 3) {
+            message += `\\- [Show more results]\(https://communitypowerea.userecho.com/search?forum_id=7&search=${escapeCharacters(query)}\)`;
+        }
+
+        await ctx.reply(message, {
+            parse_mode: 'MarkdownV2',
+            disable_web_page_preview: true
+        });
+    }
+}
+
+bot.command('help', help);
+
+bot.command('version', async (ctx) => {
+    await ctx.reply("1.0 by JuniarZ");
+});
+
+bot.on('text', async (ctx) => {
+    const { update: { message: { text, from: { id } } } } = ctx;
+
+    const state = cache.take(id);
+    if (state && state === "PENDING_QUESTION") {
 
         const query = text.replace(`\/help@${process.env.BOT_USERNAME} `, "").replace("\/help ", "");
 
@@ -126,52 +151,9 @@ const message = `🌈 How to support the project:
             });
         }
     }
+});
 
-    bot.command('help', help);
+bot.launch()
 
-    bot.command('version', async (ctx) => {
-        await ctx.reply("1.0 by JuniarZ");
-    });
-
-    bot.on('text', async (ctx) => {
-        const { update: { message: { text, from: { id } } } } = ctx;
-
-        const state = cache.take(id);
-        if (state && state === "PENDING_QUESTION") {
-
-            const query = text.replace(`\/help@${process.env.BOT_USERNAME} `, "").replace("\/help ", "");
-
-            const response = await performSearch(query);
-
-            if (response.data.status === 'success') {
-                const results = response.data.data;
-
-                if (results.length === 0) {
-                    await ctx.reply(`No topics related to '${query}' found. Try to rephrase!`);
-                    return;
-                }
-
-                let message = `Help topics related to '${query}':\n`
-                for (let i = 0; i < Math.min(results.length, 3); i++) {
-                    const _result = results[i];
-                    message += `\\- [${escapeCharacters(_result.header)}]\(${escapeCharacters(_result.url)}\)\n`;
-                }
-
-                if (results.length > 3) {
-                    message += `\\- [Show more results]\(https://communitypowerea.userecho.com/search?forum_id=7&search=${escapeCharacters(query)}\)`;
-                }
-
-                await ctx.reply(message, {
-                    parse_mode: 'MarkdownV2',
-                    disable_web_page_preview: true
-                });
-            }
-        }
-    });
-
-    bot.launch()
-
-    process.once('SIGINT', () => bot.stop('SIGINT'))
-    process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
-})();
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
